7c84b02192d1849436d4ac1d029fe6a9ed8ee96f
Stuart Aiken <stuart@trojan-lighting.co.uk>
2026-03-19 15:47:22 +0000

    Add Manchester page

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  appType: 'mpa',
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        manchester: 'manchester.html',
+      },
+    },
+  },
 })
-
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-// export default defineConfig({
-//  plugins: [react()],
-//})
